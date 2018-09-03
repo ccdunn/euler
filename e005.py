dc535ad1001ab9aae93080c35fb88f3e166df72a
Charles Dunn <charles.dunn@stresearch.com>
2018-09-03 19:56:08 -0400

    Solve 5 quite nicely

--- e005.py
+++ e005.py
@@ -9,4 +9,23 @@ What is the smallest number that is evenly divisible by all of the numbers
 from 1 to 20?
 """
 
+import utils
+import numpy as np
 
+
+def solve_0(N):
+    factors = np.array([], int)
+    pfactors = np.arange(0, N+1)
+    for n in np.arange(2, N):
+        factors = np.append(factors, pfactors[n])
+        pfactors[n::n] = pfactors[n::n] // pfactors[n]
+    return np.prod(factors)
+
+
+def solve(N):
+    return solve_0(N)
+
+
+# print(solve(10))
+assert(solve(10) == 2520)
+print(solve(20))
