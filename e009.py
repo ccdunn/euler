0ce3a37f5661639286ced72bd91157e5b2b8d463
Charles Dunn <charles.dunn@stresearch.com>
2018-09-04 08:25:54 -0400

    Solve 9

--- e009.py
+++ e009.py
@@ -11,5 +11,20 @@ For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 """
+import numpy as np
+import utils
 
+
+def solve(N):
+    b = np.arange(2, (N - 1)/3, dtype=int)
+    a = N*(N/2 - b)/(N - b)
+    ind = np.where(np.mod(a, 1) == 0)[0][0]
+    b = b[ind]
+    a = a[ind]
+    return int(a*b*np.sqrt(a**2 + b**2))
+
+
+# print(solve(12))
+assert(solve(12) == 3*4*5)
+print(solve(1000))
 
