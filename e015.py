357f938e2240cabf10b6f74981a58eb42b820768
Charles Dunn <charles.dunn@stresearch.com>
2018-09-07 11:19:10 -0400

    Just crushed 15

--- e015.py
+++ e015.py
@@ -7,5 +7,14 @@ Starting in the top left corner of a 2 * 2 grid, there are 6 routes
 
 How many routes are there through a 20 * 20 grid?
 """
+import utils
+import numpy as np
 
 
+def solve(N):
+    return int(np.math.factorial(2*N)/(np.math.factorial(N)**2))
+
+
+# print(solve(2))
+assert(solve(2) == 6)
+print(solve(20))
