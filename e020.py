cc482ba38f8f90f08e39280cfb728f87fd328044
Charles Dunn <charles.dunn@stresearch.com>
2018-09-08 23:17:56 -0400

    Solve 20 thanks to Pythons infinite integer class

--- e020.py
+++ e020.py
@@ -6,5 +6,13 @@ n! means n * (n - 1) * ... * 3 * 2 * 1
 
 Find the sum of the digits in the number 100!
 """
+import numpy as np
+import utils
 
+
+def solve(N):
+    return np.sum(utils.basify10(np.math.factorial(N)))
+
+
+print(solve(100))
 
