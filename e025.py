c6f3fa11984f2ab055884c2c97a6431d4c24a433
Charles Dunn <charles.dunn@stresearch.com>
2018-09-09 21:35:09 -0400

    Solve 25. Hallelujah for Python infinite integer class

--- e025.py
+++ e025.py
@@ -25,5 +25,27 @@ The 12th term, F[12], is the first term to contain three digits.
 
 What is the first term in the Fibonacci sequence to contain 1000 digits?
 """
+import numpy as np
+import utils
+
+# different def of fib sequence from previous problems which is annoying
 
 
+def solve(N):
+    f1 = 1
+    f2 = 1
+    t = np.zeros((N, ), dtype=int)
+    t[0] = 1
+    thresh = utils.debasify10(t)
+    count = 2
+    while f2 < thresh:
+        ftmp = f1
+        f1 = f2
+        f2 = ftmp + f2
+        count += 1
+
+    return count
+
+
+assert(solve(3) == 12)
+print(solve(1000))
