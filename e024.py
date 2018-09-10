c7a78bc1f2796fe5623759aba33f40e474c52e28
Charles Dunn <charles.dunn@stresearch.com>
2018-09-09 21:15:02 -0400

    Solve 24

--- e024.py
+++ e024.py
@@ -12,5 +12,24 @@ lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
 What is the millionth lexicographic permutation of the digits 0, 1, 2, 3,
 4, 5, 6, 7, 8 and 9?
 """
+import numpy as np
+import utils
 
+
+def solve(N, K):
+    n = N - 1
+    bases = np.zeros((K, ), dtype=int)
+    nums_left = np.arange(K, dtype=int)
+    for k in np.arange(K, dtype=int):
+        fac_k = np.math.factorial(K - 1 - k)
+        ind = n // fac_k
+        n -= ind*fac_k
+        bases[k] = nums_left[ind]
+        nums_left = np.delete(nums_left, ind)
+    # np.array([N//np.math.factorial(k - 1) for k in np.arange(K, 0, -1, dtype=int)])
+    return utils.debasify10(bases)
+
+
+# assert(solve(3, 3) == 102)
+print(solve(1000000, 10))
 
