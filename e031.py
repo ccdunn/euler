c28e6a26e93a1a1dc574d38ac756d736525e586e
Charles Dunn <charles.dunn@stresearch.com>
2018-09-12 21:57:44 -0400

    Solve 31

--- e031.py
+++ e031.py
@@ -13,5 +13,26 @@ It is possible to make -L-2 in the following way:
 
 How many different ways can -L-2 be made using any number of coins?
 """
+import numpy as np
+import utils
 
+
+british_denoms = np.array([1, 2, 5, 10, 20, 50, 100, 200])
+
+
+def count_change_rec(N, denoms):
+    if N == 0:
+        return 1
+    if not denoms.size:
+        return 0
+    return sum([count_change_rec(N - coins, denoms[:-1]) for coins in denoms[-1]*np.arange(0, N//denoms[-1] + 1, dtype=int)])
+
+
+def solve(N):
+    return count_change_rec(N, british_denoms)
+
+
+# print(solve(5))
+# assert(solve(5) == 4)
+print(solve(200))
 
